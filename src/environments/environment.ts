1448696486f615f2e70c4e6147eecece1506799d
Chloros <chloros@Chloross-MacBook-Pro.local>
2024-07-22 04:29:58 +0800

    env config3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API_URL: 'http://localhost:8080',
+    API_URL: 'https://chloroslin-spring-angular-t02-se-env.eba-mwidv9bj.ap-southeast-1.elasticbeanstalk.com',
 };
