84111a86d2a0766a25529b1a5b937dd0d09ac843
Chloros <chloros@Chloross-MacBook-Pro.local>
2024-07-22 10:18:51 +0800

    change localstorage to session storage

--- src/app/auth/services/storage/storage.service.ts
+++ src/app/auth/services/storage/storage.service.ts
@@ -7,33 +7,33 @@ export class StorageService {
 
   static saveToken(token: string): void {
     if (this.isBrowser()) {
-      window.localStorage.setItem('token', token);
+      window.sessionStorage.setItem('token', token);
     }
   }
 
   static getToken(): string | null {
     if (this.isBrowser()) {
-      return window.localStorage.getItem('token');
+      return window.sessionStorage.getItem('token');
     }
     return null;
   }
 
   static clearUserStorage(): void {
     if (this.isBrowser()) {
-      window.localStorage.removeItem('token');
-      window.localStorage.removeItem('user');
+      window.sessionStorage.removeItem('token');
+      window.sessionStorage.removeItem('user');
     }
   }
 
   static saveUser(user: User): void {
     if (this.isBrowser()) {
-      window.localStorage.setItem('user', JSON.stringify(user));
+      window.sessionStorage.setItem('user', JSON.stringify(user));
     }
   }
 
   static getUser(): User | null {
     if (this.isBrowser()) {
-      const user = window.localStorage.getItem('user');
+      const user = window.sessionStorage.getItem('user');
       return user ? JSON.parse(user) : null;
     }
     return null;
